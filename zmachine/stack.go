ef6349aa699af1b676fdfce4262c520fea48e322
Dustin Campbell <dustin@teamcampbell.org>
2015-09-24 20:00:13 -0700

    Flesh out stack implementation and add more unit test coverage

--- zmachine/stack.go
+++ zmachine/stack.go
@@ -1,5 +1,7 @@
 package zmachine
 
+import "errors"
+
 // Stack represents the program stack of a Z-Machine
 type Stack struct {
 	words    []uint16
@@ -8,15 +10,34 @@ type Stack struct {
 }
 
 // Push a value onto a stack.
-func (s *Stack) Push(value uint16) {
+func (s *Stack) Push(value uint16) (error error) {
+	if s.pointer == s.capacity {
+		return errors.New("Stack overflow")
+	}
+
 	s.words[s.pointer] = value
 	s.pointer++
+
+	return nil
 }
 
 // Pop a value off of a stack.
-func (s *Stack) Pop() uint16 {
+func (s *Stack) Pop() (result uint16, error error) {
+	if s.pointer == 0 {
+		return 0, errors.New("Stack underflow")
+	}
+
 	s.pointer--
-	return s.words[s.pointer]
+	return s.words[s.pointer], nil
+}
+
+// Peek returns the top value on a stack without popping it.
+func (s *Stack) Peek() (result uint16, error error) {
+	if s.pointer == 0 {
+		return 0, errors.New("Stack underflow")
+	}
+
+	return s.words[s.pointer-1], nil
 }
 
 // Size returns the current size of a stack.
--- zmachine/stack_test.go
+++ zmachine/stack_test.go
@@ -1,6 +1,7 @@
 package zmachine
 
 import (
+	"errors"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
@@ -36,8 +37,46 @@ func TestStackPop(t *testing.T) {
 
 	Convey("Popping a value off a stack with a size of 1, should return the value and leave stack.size equal to 0", t, func() {
 		s.Push(42)
-		v := s.Pop()
+		v, _ := s.Pop()
 		So(v, ShouldEqual, 42)
 		So(s.Size(), ShouldEqual, 0)
+	})
+}
+
+func TestStackPeek(t *testing.T) {
+	s := NewStack(10)
+
+	Convey("Peeking at a value on stack with a size of 1, should return the value and leave stack.size equal to 1", t, func() {
+		s.Push(42)
+		v, _ := s.Peek()
+		So(v, ShouldEqual, 42)
+		So(s.Size(), ShouldEqual, 1)
+	})
+}
+
+func TestStackPopUnderflow(t *testing.T) {
+	s := NewStack(10)
+
+	Convey("Popping a value off an empty stack returns an error", t, func() {
+		_, e := s.Pop()
+		So(e, ShouldResemble, errors.New("Stack underflow"))
+	})
+}
+
+func TestStackPeekUnderflow(t *testing.T) {
+	s := NewStack(10)
+
+	Convey("Peeking at a value on an empty stack returns an error", t, func() {
+		_, e := s.Peek()
+		So(e, ShouldResemble, errors.New("Stack underflow"))
+	})
+}
+
+func TestStackPushOverflow(t *testing.T) {
+	s := NewStack(0)
+
+	Convey("Pushing a value onto an empty stack with a capacity of 0 returns an error", t, func() {
+		e := s.Push(42)
+		So(e, ShouldResemble, errors.New("Stack overflow"))
 	})
 }
