732d44ed63367cac153387b5cf31988a25d6e5be
Dustin Campbell <dustin@teamcampbell.org>
2015-09-25 17:43:46 -0700

    Add APIs to retrieve addresses from the header

--- zmachine/memory.go
+++ zmachine/memory.go
@@ -5,9 +5,39 @@ type Memory struct {
 	bytes []byte
 }
 
+func (m *Memory) readByte(index int) byte {
+	return m.bytes[index]
+}
+
+func (m *Memory) readWord(index int) uint16 {
+	return uint16(m.bytes[index])<<8 | uint16(m.bytes[index+1])
+}
+
+const (
+	versionOffset               = 0x00
+	highMemoryBaseOffset        = 0x04
+	initialProgramCounterOffset = 0x06
+	staticMemoryBaseOffset      = 0x0e
+)
+
 // Version returns the version number from Z-Machine memory.
 func (m *Memory) Version() byte {
-	return m.bytes[0]
+	return m.readByte(versionOffset)
+}
+
+// HighMemoryBase returns the byte address of the base of high memory.
+func (m *Memory) HighMemoryBase() uint16 {
+	return m.readWord(highMemoryBaseOffset)
+}
+
+// StaticMemoryBase returns the byte address of the base of high memory.
+func (m *Memory) StaticMemoryBase() uint16 {
+	return m.readWord(staticMemoryBaseOffset)
+}
+
+// InitialProgramCounter returns the byte address of initial program counter.
+func (m *Memory) InitialProgramCounter() uint16 {
+	return m.readWord(initialProgramCounterOffset)
 }
 
 // NewMemory creates an instance of Memory.
--- zmachine/memory_test.go
+++ zmachine/memory_test.go
@@ -38,9 +38,21 @@ func createZork1Memory() Memory {
 	return NewMemory(buffer)
 }
 
-func TestVersion_Zork1(t *testing.T) {
+func TestHeader_Zork1(t *testing.T) {
 	memory := createZork1Memory()
 	Convey("Zork 1: version should be 3", t, func() {
 		So(memory.Version(), ShouldEqual, 3)
+	})
+
+	Convey("Zork 1: static memory base should be 0x2e53", t, func() {
+		So(memory.StaticMemoryBase(), ShouldEqual, 0x2e53)
+	})
+
+	Convey("Zork 1: high memory base should be 0x4e37", t, func() {
+		So(memory.HighMemoryBase(), ShouldEqual, 0x4e37)
+	})
+
+	Convey("Zork 1: initial program counter should be 0x4f05", t, func() {
+		So(memory.InitialProgramCounter(), ShouldEqual, 0x4f05)
 	})
 }
